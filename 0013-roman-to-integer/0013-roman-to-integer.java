7e8175bdc2662821f4a3cc8294f2cf372da02192
Yashwanth Gowda AV <yashwanthavyashwanthav813@gmail.com>
2026-02-11 22:36:10 +0530

    Time: 6 ms (9.36%), Space: 46.9 MB (28.28%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int romanToInt(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int value = map.get(s.charAt(i));
+
+            // If next symbol is bigger → subtract
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+                result -= value;
+            } 
+            // Else add normally
+            else {
+                result += value;
+            }
+        }
+
+        return result;
+    }
+}
