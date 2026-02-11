7d547c118c77bea3364fa2941a679a405df9b5cb
Yashwanth Gowda AV <yashwanthavyashwanthav813@gmail.com>
2026-02-11 22:22:40 +0530

    Time: 5 ms (68.4%), Space: 43.6 MB (55.34%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int hIndex(int[] citations) {
+
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int papers = n - i;
+
+            if (citations[i] >= papers) {
+                return papers;
+            }
+        }
+
+        return 0;
+    }
+}
